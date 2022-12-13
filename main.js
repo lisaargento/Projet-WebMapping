28888d46d545cab97d7b91251a785e6a444b1e12
lisa.argento <lisa.argento@ensg.eu>
2022-12-13 12:10:01 +0100

    ajout départements france métropole

--- main.js
+++ main.js
@@ -3,44 +3,63 @@ var select_statistique = document.getElementById('statistiques');
 var select_annee = document.getElementById('annee');
 var form = document.getElementById('form');
 
+
 // CREATION CARTE AVEC OPENLAYERS
 var map = new ol.Map({
     target: 'map', // the ID of the element in which to render the map
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
-        
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 5
+      center: ol.proj.fromLonLat([3, 47]),
+      zoom: 5.5
     })
   });
+
+// AJOUT GEOJSON DES DÉPARTEMENTS -> create a new vector layer for the GeoJSON data
+  var dep = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      url: 'contour_dep.geojson',
+      format: new ol.format.GeoJSON()
+    }),
+  })
+  map.addLayer(dep);// add the vector layer to the map
+
 
 
 // AJOUT DES STATISTIQUES DANS LE SELECT
 
-select_statistiques.addEventListener('change', function(e){
-  select_statistique.selectedIndex=1;
-  fetch('donnees.json')
-  .then(function(response){
-    if (response.status!==200){
-      console.log('Error Satus Code :'+response.status);
-      return;
+select_statistique.selectedIndex=1;
+fetch('donnees.json').then(function(response){
+  if (response.status!==200){
+    console.log('Error Satus Code :'+ response.status);
+    return;
+  }
+  response.json().then(function(data){
+    let option;
+    console.log(data[1].statistiques);
+    for (let i = 0; i < data[1].statistiques.length; i++){
+      option = document.createElement('option');
+      option.text = data[1].statistiques[i].nom;
+      select_statistique.add(option);
     }
-    response.json().then(function(data){
-      let option;
-      console.log(data[1].statistiques)
-      for (let i=0; i<data[1].statistiques.length; i++){
-        option = document.createElement('option');
-        option.text = data[1].statistiques[i].nom;
-        select_statistique.add(option);
-      }
-    })
-  })
-  .then(r => {
-    // console.log(r)
-    select_statistiques.innerHTML = r;
   })
 })
+
+//AJOUT DES ANNEES DANS LE SELECT
+// for (let i = 1980; i < 2023; i++){
+//   option = document.createElement('option');
+//   option.text = i;
+//   select_annee.add(option);
+// }
+
+form.addEventListener("submit", envoi)
+function envoi(e){
+  e.preventDefault();
+  var stat = select_statistique.options[select_statistique.selectedIndex].value;
+  var annee = select_annee.value;
+  console.log("stats : ",stat);
+  console.log("année : ",annee);
+}
