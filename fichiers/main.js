35b4c5a6e1f96ebd335e1af12b4ab5d5b926e363
lisa.argento <lisa.argento@ensg.eu>
2022-12-28 16:07:58 +0100

    modif

--- fichiers/main.js
+++ fichiers/main.js
@@ -8,8 +8,10 @@ var titre = document.getElementById('titre');
 var timeline = document.getElementById('timeline');
 var stat;
 var annee;
+var id_stat;
+var nom_dep;
 var tab;
-var dep_select;
+var PannelAlreadyExist = false;
 
 
 // CREATION CARTE AVEC OPENLAYERS
@@ -34,8 +36,8 @@ var dep = new ol.layer.Vector({
   source: new ol.source.Vector({
     url: 'contour_dep.geojson',
     format: new ol.format.GeoJSON(),
-  }),
-})
+  })
+});
 map.addLayer(dep);// add the vector layer to the map
 console.log(dep);
 
@@ -45,7 +47,7 @@ console.log(dep);
 select_statistique.selectedIndex=1;
 fetch('donnees.json').then(function(response){
   if (response.status!==200){
-    console.log('Error Satus Code :'+ response.status);
+    console.log('Error Satus Code :' + response.status);
     return;
   }
   response.json().then(function(data){
@@ -66,7 +68,7 @@ for (let i = 2022; i > 1980; i--){
 }
 
 
-var id_stat = -1; //id stat choisie
+
 //ENVOIE FORM AVEC STAT ET ANNEE CHOISIES
 form.addEventListener("submit", envoi)
 function envoi(e){
@@ -82,73 +84,83 @@ function envoi(e){
   return
 }
 
-// SELECTION UN DEPARTEMENT -> retourne infos
 
-  //RECUPARATION INFORMATIONS PERTINENTES
+
+
+//RECUPARATION INFORMATIONS PERTINENTES
+
+var id_stat = -1; //id stat choisie
+//récupération département cliqué
 map.on('singleclick', function(e) {
   var dep = map.forEachFeatureAtPixel(e.pixel, function(dep) {
-    if (id_stat < 0) {alert('Vous devez choisir une statistique à étudier ou valider votre choix !')}
-    return dep
+  if (id_stat < 0) {alert('Vous devez choisir une statistique à étudier ou valider votre choix !')}
+    //renvoie le nom du département cliqué dans la console
+  else{
+    nom_dep = dep.N.nom;
+    console.log("Information sur le point cliqué : ", dep.N.nom)}
+  return 
   });
-
-  //renvoie le nom du département cliqué dans la console
-//console.log("Information sur le point cliqué : ", dep.N.nom); 
-
-  //Affiche le tableau associé au département choisi
-fetch('donnees.json').then(function(response){
-  response.json().then(function(data){
-    for (let i = 0; i < data[0].departements.length; i++){//Parcourt la liste des départements
-      // Récupère la stat en fonction du département
-      if(dep.N.nom == data[0].departements[i].nom){
-        tab = data[0].departements[i].statistiques[id_stat];
-        console.log(tab); // tableau 12,1 en sortie qui n'est pas affichable
-        return tab
-      }
-    }
-  })
-})
-
-  //AFFICHAGE INFORMATIONS PERTINENTES -> panneau pour Timeline 
-let PannelAlreadyExist = false
-
-  //création panneau s'il n'xuste pas déjà
-if( tab != "" && ! PannelAlreadyExist) {
-  setTimeout( () => {
-    pannel.style.display = 'block'
-  }, 2000 )
-  PannelAlreadyExist = true
-}
-
-  //ajout Info stat étudiée titre pannel (NE FONCTIONNE PAS)
-titre.innerHTML = dep.N.nom.bold() + " : Timeline circulaire du " + stat.toLowerCase() + " en " + annee;
-timeline.add(Janvier);
-//var month = new Array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");  
-// for (i = 0; i<month.length; i++){  
-// timeline.add(month[i]);  
-// }
-
-  //fermeture
-  document.getElementById('btn_close').addEventListener('click', function(e) {
-  pannel.style.display = 'none'
-  })
-
 });
 
+//Récupérer le tableau associé au département choisi
+  fetch('donnees.json').then(function(response){
+    response.json().then(function(data){
+      for (let i = 0; i < data[0].departements.length; i++){//Parcourt la liste des départements
+        // Récupère la stat en fonction du département
+        if(nom_dep == data[0].departements[i].nom){
+          tab = data[0].departements[i].statistiques[id_stat];
+          console.log(tab); // tableau 12,1 en sortie qui n'est pas affichable
+        return tab
+        }
+      }
+    })
+  })
 
- 
 
 
 
-// function fetchJSON(url, code_dep) { //Appel du GEOJSON stocké dans le dossier, contenant les frontières de chaque département métropolitain
-//   return fetch(url)
-//   .then(result => result.json())
-//   .then(r => {
-//     for (var i = 0; i <= 96; i++) {
-//       if (r['features'][i]['properties']['code'] == code_dep) { //Grâce au 'for... if...', on trouve les frontières correspondant au département souhaité.
-//         L.geoJSON(r['features'][i]).addTo(dep_layer); //Ajout des frontières dans le layerGroup
-//         dep_layer.addTo(map); //Ajout du layerGroup sur la carte
-//       }
-//     }
-//   });
-//   }
-//fetchJSON('../contour-des-departements.geojson', r[0]['code_dep'])
+  
+    //AFFICHAGE INFORMATIONS PERTINENTES -> panneau pour Timeline 
+function afficher_pannel(){ //NE FONCTIONNE PAS !!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //création panneau s'il n'existe pas déjà
+  if(PannelAlreadyExist = false) {
+    setTimeout( () => {
+      pannel.style.display = 'block'
+    }, 1000 )
+
+    //ajout Info stat étudiée titre pannel 
+    titre.innerHTML = nom_dep.bold() + " : Timeline circulaire du " + stat.toLowerCase() + " en " + annee;
+    timeline.add(Janvier);//(NE FONCTIONNE PAS)
+    //var month = new Array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");  
+    // for (i = 0; i<month.length; i++){  
+    // timeline.add(month[i]);  
+    // }
+
+    //fermeture
+    document.getElementById('btn_close').addEventListener('click', function(e) {
+      pannel.style.display = 'none'
+      });
+
+    PannelAlreadyExist = true
+  }
+
+}
+  
+   
+  
+  
+  
+  // function fetchJSON(url, code_dep) { //Appel du GEOJSON stocké dans le dossier, contenant les frontières de chaque département métropolitain
+  //   return fetch(url)
+  //   .then(result => result.json())
+  //   .then(r => {
+  //     for (var i = 0; i <= 96; i++) {
+  //       if (r['features'][i]['properties']['code'] == code_dep) { //Grâce au 'for... if...', on trouve les frontières correspondant au département souhaité.
+  //         L.geoJSON(r['features'][i]).addTo(dep_layer); //Ajout des frontières dans le layerGroup
+  //         dep_layer.addTo(map); //Ajout du layerGroup sur la carte
+  //       }
+  //     }
+  //   });
+  //   }
+  //fetchJSON('../contour-des-departements.geojson', r[0]['code_dep'])
+  
