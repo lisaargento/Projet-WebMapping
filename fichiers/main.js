ce25452549dbbbcff83a933054d777a372728f7d
lisa.argento <lisa.argento@ensg.eu>
2022-12-29 15:41:24 +0100

    construction graph 2d en js

--- fichiers/main.js
+++ fichiers/main.js
@@ -147,10 +147,48 @@ function afficher_pannel(){ //NE FONCTIONNE PAS !!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 }
   
-   
+//CODE HTML pour graph des stats
+// <!DOCTYPE html>
+// <html>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+// <body>
+// <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+
+//CODE JS pour graph des stats
+// <script>
+// var xValues = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
+// var yValues = [7,8,8,9,9,9,10,11,14,14,15,15];
+
+// new Chart("myChart", {
+//   type: "line",
+//   data: {
+//     labels: xValues,
+//     datasets: [{
+//       fill: false,
+//       lineTension: 0,
+//       backgroundColor: "rgba(0,0,255,1.0)",
+//       borderColor: "rgba(0,0,255,0.1)",
+//       data: yValues
+//     }]
+//   },
+//   options: {
+//     legend: {display: false},
+//     scales: {
+//       yAxes: [{ticks: {min: Math.min.apply(null, yValues)-1, max: Math.max.apply(null, yValues)+1}}],
+//     }
+//   }
+// });
+// </script>
+
+// </body>
+// </html>
+
+
+
   
-  
-  
+
+
+
   // function fetchJSON(url, code_dep) { //Appel du GEOJSON stocké dans le dossier, contenant les frontières de chaque département métropolitain
   //   return fetch(url)
   //   .then(result => result.json())
