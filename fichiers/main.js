8c799843c6235e199e5b1394cfd26b40729d331b
lisa.argento <lisa.argento@ensg.eu>
2023-01-10 14:23:57 +0100

    affichage pannel erreur fermeture

--- fichiers/main.js
+++ fichiers/main.js
@@ -8,10 +8,11 @@ var titre = document.getElementById('titre');
 var timeline = document.getElementById('timeline');
 var stat;
 var annee;
-var id_stat;
+var id_stat = -1;
 var nom_dep;
 var tab;
-var PannelAlreadyExist = false;
+var PannelAlreadyExist = 0;
+//var PannelAlreadyExist = new Boolean(false);   // valeur initiale à false;
 
 
 // CREATION CARTE AVEC OPENLAYERS
@@ -78,7 +79,7 @@ function envoi(e){
   id_stat = select_statistique.selectedIndex - 1;
   // console.log("stats : ",stat);
   // console.log("année : ",annee);
-  // console.log("id_stat : ",id_stat);
+  console.log("id_stat : ",id_stat);
   //Affiche le nom de la stat étudiée
   info.innerHTML = "Statistique étudiée : " + stat + " en " + annee;
   return
@@ -89,34 +90,32 @@ function envoi(e){
 
 //RECUPARATION INFORMATIONS PERTINENTES
 
-var id_stat = -1; //id stat choisie
+
 //récupération département cliqué
 map.on('singleclick', function(e) {
-  var dep = map.forEachFeatureAtPixel(e.pixel, function(dep) {
+  dep = map.forEachFeatureAtPixel(e.pixel, function(dep) {
   if (id_stat < 0) {alert('Vous devez choisir une statistique à étudier ou valider votre choix !')}
-    //renvoie le nom du département cliqué dans la console
   else{
+    //Renvoie le nom du département cliqué dans la console
     nom_dep = dep.N.nom;
-    //console.log("Information sur le point cliqué : ", dep.N.nom)
-  }
-  return 
-  });
-});
-
-//Récupérer le tableau associé au département choisi
-  fetch('donnees.json').then(function(response){
-    response.json().then(function(data){
-      for (let i = 0; i < data[0].departements.length; i++){//Parcourt la liste des départements
-        // Récupère la stat en fonction du département
-        if(nom_dep == data[0].departements[i].nom){
-          tab = data[0].departements[i].statistiques[id_stat];
-          console.log(tab); // tableau 12,1 en sortie qui n'est pas affichable
-        afficher_pannel();
-        return 
+    console.log("Information sur le point cliqué : ", dep.N.nom);
+    //Récupére le tableau associé au département choisi
+    fetch('donnees.json').then(function(response){
+      response.json().then(function(data){
+        for (let i = 0; i < data[0].departements.length; i++){//Parcourt la liste des départements
+          // Récupère la stat en fonction du département
+          if(nom_dep == data[0].departements[i].nom){
+            tab = data[0].departements[i].statistiques[id_stat];
+            console.log(tab); // tableau 12,1 en sortie qui n'est pas affichable
+          }
         }
-      }
+      })
     })
+    //Affiche pannel 
+    afficher_pannel();
+  }
   })
+})
 
 
 
@@ -124,25 +123,27 @@ map.on('singleclick', function(e) {
     //AFFICHAGE INFORMATIONS PERTINENTES -> panneau pour Timeline 
 function afficher_pannel(){ //NE FONCTIONNE PAS !!!!!!!!!!!!!!!!!!!!!!!!!!!
     //création panneau s'il n'existe pas déjà
-  if(PannelAlreadyExist = false) {
-    setTimeout( () => {
-      pannel.style.display = 'block'
+  if(PannelAlreadyExist == 0) {
+    setTimeout( function(){
+      pannel.style.display = 'block';
     }, 1000 )
 
     //ajout Info stat étudiée titre pannel 
     titre.innerHTML = nom_dep.bold() + " : Timeline circulaire du " + stat.toLowerCase() + " en " + annee;
-    timeline.add(Janvier);//(NE FONCTIONNE PAS)
-    //var month = new Array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");  
+    var month = new Array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");  
+    timeline.innerHTML = month;// CA SORT DU PANNEAU!!!!
     // for (i = 0; i<month.length; i++){  
     // timeline.add(month[i]);  
     // }
 
     //fermeture
-    document.getElementById('btn_close').addEventListener('click', function(e) {
-      pannel.style.display = 'none'
-      });
+    document.getElementById('btn_close').addEventListener('click', function() {
+      //pannel.style.display = 'none';
+      //pannel.close(); //NE FONCTIONNE PAS
+      PannelAlreadyExist = 0
+    });
 
-    PannelAlreadyExist = true
+    PannelAlreadyExist = 1
   }
 
 }
