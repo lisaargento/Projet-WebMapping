d308b11a70790dea7d156f797dc509957dbfaa30
lisa.argento <lisa.argento@ensg.eu>
2023-02-26 10:42:59 +0000

    chart interactive!!

--- fichiers/main.js
+++ fichiers/main.js
@@ -6,6 +6,9 @@ var info = document.getElementById('info');
 var pannel = document.getElementById('pannel');
 var titre = document.getElementById('titre');
 var timeline = document.getElementById('timeline');
+var startAnimation = document.getElementById("startAnimation");
+var stopAnimation = document.getElementById("stopAnimation");
+var btn_close = document.getElementById("btn_close");
 var stat;
 var annee;
 var id_stat = -1;
@@ -13,8 +16,9 @@ var nom_dep;
 var tab;
 var PannelAlreadyExist = 0;
 var chart;
+var t;
+var i = 2; // indice pour faire défiler les points de la chart
 
-//alert("Vous devez d'abord choisir une statistique à étudier et une année d'étude. Puis il vous suffira de cliquer sur le ou les départements que vous souhaitez étudier.");
 
 
 
@@ -83,7 +87,7 @@ depLayer.setStyle(styleFunction);
 //  -------------------------- REMPLISSAGE ET ENVOI FORM -------------------------- //
 
 // AJOUT DES STATISTIQUES DANS LE SELECT
-select_statistique.selectedIndex=1;
+//select_statistique.selectedIndex=1; // à garder ???
 fetch('donnees.json').then(function(response){
   if (response.status!==200){
     console.log('Error Satus Code :' + response.status);
@@ -111,15 +115,20 @@ for (let i = 2022; i > 1980; i--){
 form.addEventListener("submit", envoi)
 function envoi(e){
   e.preventDefault();
-  stat = select_statistique.options[select_statistique.selectedIndex].value;
-  annee = select_annee.value;
-  id_stat = select_statistique.selectedIndex - 1;
-  // console.log("stats : ",stat);
-  // console.log("année : ",annee);
-  console.log("id_stat : ",id_stat);
-  //Affiche le nom de la stat étudiée
-  info.innerHTML = "Statistique étudiée : " + stat + " en " + annee;
-  return
+  if (select_statistique.value == 'Choix de la statistique' || select_annee.value == "Choix de l'année d'étude") {
+    alert("Vous devez d'abord choisir une statistique à étudier et une année d'étude. Puis il vous suffira de cliquer sur le ou les départements que vous souhaitez étudier.");
+  }
+  else {
+    stat = select_statistique.options[select_statistique.selectedIndex].value;
+    annee = select_annee.value;
+    id_stat = select_statistique.selectedIndex - 1;
+    // console.log("stats : ",stat);
+    // console.log("année : ",annee);
+    console.log("id_stat : ",id_stat);
+    //Affiche le nom de la stat étudiée
+    info.innerHTML = "Statistique étudiée : " + stat + " en " + annee;
+  }
+
 }
 
 
@@ -129,38 +138,32 @@ function envoi(e){
 //récupération département cliqué
 map.on('singleclick', function(e) {
   dep = map.forEachFeatureAtPixel(e.pixel, function(dep) {
-  if (id_stat < 0) {alert('Vous devez choisir une statistique à étudier ou valider votre choix !')}
-  else{
-    nom_dep = dep.N.nom;
-    console.log("Information sur le point cliqué : ", dep.N.nom);//Renvoie le nom du département cliqué dans la console
-    //Récupére le tableau associé au département choisi
-    fetch('donnees.json').then(function(response){
-      response.json().then(function(data){
-        for (let i = 0; i < data[0].departements.length; i++){//Parcourt la liste des départements
-          // Récupère la stat en fonction du département
-          if(nom_dep == data[0].departements[i].nom){
-            tab = data[0].departements[i].statistiques[id_stat],
-            console.log(tab); // tableau 12,1 en sortie
+    if (id_stat < 0) {
+      alert('Vous devez choisir une statistique à étudier ou valider votre choix !')
+    }
+    else {
+      nom_dep = dep.N.nom;
+      console.log("Information sur le point cliqué : ", dep.N.nom);//Renvoie le nom du département cliqué dans la console
+      //Récupére le tableau associé au département choisi
+      fetch('donnees.json').then(function(response){
+        response.json().then(function(data){
+          for (let i = 0; i < data[0].departements.length; i++){//Parcourt la liste des départements
+            // Récupère la stat en fonction du département
+            if(nom_dep == data[0].departements[i].nom){
+              tab = data[0].departements[i].statistiques[id_stat],
+              console.log(tab); // tableau 12,1 en sortie
+            }
           }
-        }
-        afficher_pannel();
-      })
-    })    
-  };
+          // Affichage du panneau
+          pannel.style.display = 'block';
+          remplissage_pannel();
+        })
+      })    
+    };
   })
 })
 
 
-//AFFICHAGE PANNEAU  
-function afficher_pannel(){ 
-  //création panneau s'il n'existe pas déjà
-  if(PannelAlreadyExist == 0) {
-    pannel.style.display = 'block',
-    PannelAlreadyExist = 1,
-    fermer_pannel(),
-    remplissage_pannel()
-  }
-}
 
 //AJOUT INFORMATIONS DANS PANNEAU
 function remplissage_pannel(){
@@ -168,8 +171,6 @@ function remplissage_pannel(){
   titre.innerHTML = nom_dep.bold() + " : Timeline du " + stat.toLowerCase() + " en " + annee;
 
   var xValues = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
-  var yValues = tab;
-  
 
   chart = new Chart("myChart", {
     type: "line",
@@ -182,61 +183,52 @@ function remplissage_pannel(){
         borderColor: "rgba(0,20,255,0.5)",
         strokeColor: "rgba(151,187,205,1)",
         pointColor: "rgba(0,20,255,0.5)",
-        data: [yValues[0]]}    
+        data: [tab[0]]}    
       ]
     },
     options: {
       scales: {
-        yAxes: [{ticks: {min: 0, max: Math.max.apply(null, tab)+1}}],
+        y: {
+          suggestedMin: 0,
+          suggestedMax: Math.max.apply(null, tab) + Math.min.apply(null, tab)
+        }
       }
     }
   });
-  
-  // Add a new point to the chart every 2 seconds
 
-  // var i=0;
-  // let pointInterval = setInterval(
-  //     i+=1,
-  //     chart.data.datasets[0].data.push(yValues[i]),
-  //     chart.update(),
-  //     // if (i=12){break},// Update the chart to display the new data point every 2 seconds 
-  //   1000); // Add the new data point to the chart
-  //   if (i=12){clearInterval(pointInterval)};
+  i = 2; // initialisation obligatoire si on veut pouvoir fermer et ouvrir correctement des panneaux
+  affichagePoints();
 
-//   for (let i = 0; i < 11; i++){//for (var i = 0; i < yValues.length; i++){
-//     chart.update();// Update the chart to display the new data point every 2 seconds
-//     setTimeout(
-//       chart.data.datasets[0].data.push(yValues[i]),
-//     1000); // Add the new data point to the chart
-//  };
 }
   
-
-  //SUPPRESSION PANNEAU
-  function fermer_pannel(){
-    //écoute bouton pour fermer panneau
-    if(PannelAlreadyExist == 1) {
-      document.getElementById("btn_close").addEventListener('click', function() {
-        chart.destroy();//supprimer chart existante!!!!!!!!!!!!!!!!!!!!!
-        pannel.style.display = 'none';
-        PannelAlreadyExist = 0;
-      });
+// Permet d'afficher les points au fur et à mesure
+function affichagePoints() {
+  t = setInterval(function () {
+    if (i == 13) {
+      i = 1;
     }
-  };
+    chart.data.datasets[0].data = tab.slice(0,i); // .slice équivaut que slicing en python tableau[0:3] => renvoie les 3 premières valeurs du tableau i.e. tableau[0], tableau[1], tableau[2]
+    chart.update();
+    i ++;
+  } , 1000);
+}
 
 
-//Pr colorer les départements??????????????????????????????
-  // function fetchJSON(url, code_dep) { //Appel du GEOJSON stocké dans le dossier, contenant les frontières de chaque département métropolitain
-  //   return fetch(url)
-  //   .then(result => result.json())
-  //   .then(r => {
-  //     for (var i = 0; i <= 96; i++) {
-  //       if (r['features'][i]['properties']['code'] == code_dep) { //Grâce au 'for... if...', on trouve les frontières correspondant au département souhaité.
-  //         L.geoJSON(r['features'][i]).addTo(dep_layer); //Ajout des frontières dans le layerGroup
-  //         dep_layer.addTo(map); //Ajout du layerGroup sur la carte
-  //       }
-  //     }
-  //   });
-  //   }
-  //fetchJSON('../contour-des-departements.geojson', r[0]['code_dep'])
-  
+btn_close.addEventListener('click', function() {
+  clearInterval(t);
+  pannel.style.display = 'none';
+  chart.destroy();//supprimer chart existante!!!!!!!!!!!!!!!!!!!!!
+});
+
+startAnimation.addEventListener('click', function() {
+  clearInterval(t);
+  affichagePoints();
+});
+
+stopAnimation.addEventListener('click', function() {
+  clearInterval(t);
+});
+
+
+
+
